44804408097df3cd304b025ec5cca01499ea3ed9
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-03-18 15:22:05 +0100

    feat(surf-chat): enhance chat message rendering with item placeholders

--- surf-chat/src/main/java/dev/slne/surf/social/chat/listener/PlayerAsyncChatListener.kt
+++ surf-chat/src/main/java/dev/slne/surf/social/chat/listener/PlayerAsyncChatListener.kt
@@ -11,17 +11,23 @@ import dev.slne.surf.social.chat.util.MessageBuilder
 import dev.slne.surf.social.chat.util.Permission
 import dev.slne.surf.surfapi.bukkit.api.SurfBukkitApi
 import dev.slne.surf.surfapi.core.api.SurfCoreApi
+import dev.slne.surf.surfapi.core.api.messages.Colors
 import dev.slne.surf.surfapi.core.api.util.random
 
 import io.papermc.paper.event.player.AsyncChatEvent
+import kotlinx.coroutines.flow.merge
 
 import me.clip.placeholderapi.PlaceholderAPI
 import net.kyori.adventure.audience.Audience
+import net.kyori.adventure.key.Key
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextReplacementConfig
+import net.kyori.adventure.text.event.HoverEvent
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.entity.Player
 
 import org.bukkit.event.EventHandler
@@ -32,7 +38,7 @@ class PlayerAsyncChatListener : Listener {
     @EventHandler
     fun onChat(event: AsyncChatEvent) {
         val player = event.player
-        var plainMessage = PlainTextComponentSerializer.plainText().serialize(event.message())
+        var plainMessage = PlainTextComponentSerializer.plainText().serialize(event.message().parseItemPlaceholder(player))
 
         if (ChatFilterService.containsLink(event.message())) {
             event.isCancelled = true
@@ -96,7 +102,7 @@ class PlayerAsyncChatListener : Listener {
             return
         }
 
-        event.renderer {source, _, _, viewer ->
+        event.renderer { source, _, _, viewer ->
             Components.getDeleteComponent(viewer.toPlayer(), messageID)
                 .append(Components.getTeleportComponent(viewer.toPlayer(), source.name))
                 .append(MiniMessage.miniMessage().deserialize(PlaceholderAPI.setPlaceholders(source, "%luckperms_prefix% %player_name%")))
@@ -115,4 +121,26 @@ class PlayerAsyncChatListener : Listener {
     private fun getCountedPlayers(): Int {
         return Bukkit.getOnlinePlayers().count {!it.hasPermission(SurfChatPermissions.chatLimitBypass) }
     }
+
+    private fun Component.parseItemPlaceholder(player: Player): Component {
+        if (!PlainTextComponentSerializer.plainText().serialize(this).contains("[item]")) {
+            return this
+        }
+
+        val stack = player.inventory.itemInMainHand
+
+        if (stack.type == Material.AIR) {
+            return this
+        }
+
+        return this.replaceText(TextReplacementConfig.builder()
+            .match("[item]")
+            .replacement(Component.text(when {
+                stack.amount > 1 -> "${stack.amount}x ${stack.type.name.lowercase().replace("_", " ")}"
+                else -> stack.type.name.lowercase().replace("_", " ")
+            }, Colors.VARIABLE_VALUE).hoverEvent(stack.asHoverEvent()))
+            .build()
+        )
+    }
+
 }
