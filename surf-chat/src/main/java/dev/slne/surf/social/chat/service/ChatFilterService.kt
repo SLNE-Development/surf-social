790bd7ad718a4b6b897bc07cc4cf84a4757d414e
BuildTools <unconfigured@null.spigotmc.org>
2025-03-28 09:38:11 +0100

    Removed unnecessary Code

--- surf-chat/src/main/java/dev/slne/surf/social/chat/service/ChatFilterService.kt
+++ surf-chat/src/main/java/dev/slne/surf/social/chat/service/ChatFilterService.kt
@@ -27,13 +27,6 @@ object ChatFilterService {
     
     private val allowedDomains = mutableObjectSetOf<String>()
 
-    private val blockedPatterns = mutableObjectSetOf<Regex>()
-    private val rateLimit =
-        mutableObject2LongMapOf<UUID>().apply { defaultReturnValue(0) }.synchronize()
-
-    private val messageCount =
-        mutableObject2IntMapOf<UUID>().apply { defaultReturnValue(0) }.synchronize()
-
     private val validCharactersRegex = "^[a-zA-Z0-9/.:_,()%&=?!<>|#^\"²³+*~-äöü@ ]*$".toRegex()
     private val urlRegex =
         "((http|https|ftp)://)?([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?".toRegex(RegexOption.IGNORE_CASE)
