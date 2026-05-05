34a2ec68e19b50ee365d66b94a0b15bd70043605
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-05-05 19:09:06 +0200

    refactor: optimize UUID encryption by using lazy initialization for key and encoder

--- surf-social-velocity/src/main/kotlin/dev/slne/surf/social/velocity/util/security-util.kt
+++ surf-social-velocity/src/main/kotlin/dev/slne/surf/social/velocity/util/security-util.kt
@@ -1,18 +1,24 @@
 package dev.slne.surf.social.velocity.util
 
+import dev.slne.surf.api.core.util.random
 import dev.slne.surf.social.velocity.config
-import java.security.SecureRandom
 import java.util.*
 import javax.crypto.Cipher
 import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
-fun encryptUuid(uuid: UUID): String {
+private val key by lazy {
     val keyBytes =
         Base64.getDecoder().decode(config.encryptionSecret)
-    val key = SecretKeySpec(keyBytes, "AES")
+    SecretKeySpec(keyBytes, "AES")
+}
 
-    val nonce = ByteArray(12).also { SecureRandom().nextBytes(it) }
+private val encoder by lazy {
+    Base64.getUrlEncoder().withoutPadding()
+}
+
+fun encryptUuid(uuid: UUID): String {
+    val nonce = ByteArray(12).also { random.nextBytes(it) }
 
     val cipher = Cipher.getInstance("AES/GCM/NoPadding")
     cipher.init(Cipher.ENCRYPT_MODE, key, GCMParameterSpec(128, nonce))
@@ -20,5 +26,5 @@ fun encryptUuid(uuid: UUID): String {
     val ciphertext = cipher.doFinal(uuid.toString().toByteArray(Charsets.UTF_8))
 
     val combined = nonce + ciphertext
-    return Base64.getUrlEncoder().withoutPadding().encodeToString(combined)
+    return encoder.encodeToString(combined)
 }
