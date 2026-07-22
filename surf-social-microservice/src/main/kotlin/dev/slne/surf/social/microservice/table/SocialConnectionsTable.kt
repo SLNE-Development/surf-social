8300d9bc0b7d271995c00aa6e2a75929f3fba9dd
twisti-dev <76837088+twisti-dev@users.noreply.github.com>
2026-07-22 23:47:10 +0200

    🔧 chore: update version to 1.1.7 and rename social_connections table
    - increment version from 1.1.6 to 1.1.7 in gradle.properties
    - rename SocialConnectionsTable from "social_connections_new" to "social_connections"

--- surf-social-microservice/src/main/kotlin/dev/slne/surf/social/microservice/table/SocialConnectionsTable.kt
+++ surf-social-microservice/src/main/kotlin/dev/slne/surf/social/microservice/table/SocialConnectionsTable.kt
@@ -3,7 +3,7 @@ package dev.slne.surf.social.microservice.table
 import dev.slne.surf.database.columns.nativeUuid
 import dev.slne.surf.database.table.AuditableLongIdTable
 
-object SocialConnectionsTable : AuditableLongIdTable("social_connections_new") {
+object SocialConnectionsTable : AuditableLongIdTable("social_connections") {
     val minecraftUuid = nativeUuid("minecraft_uuid").uniqueIndex()
     val discordUserId = long("discord_user_id").uniqueIndex().nullable()
     val twitchId = long("twitch_id").uniqueIndex().nullable()
